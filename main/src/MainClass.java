263eb8d7855e9284ed07747bc55a5ea8076eb22a
mahasamadhi <bficara@outlook.com>
2017-07-08 18:15:26 -0400

    Add a third case to exit app. SF 7/8/17

--- main/src/MainClass.java
+++ main/src/MainClass.java
@@ -1,38 +1,52 @@
+import java.io.*;
+import java.util.Scanner;
+
 /**
  * Created by bfica on 7/8/2017.
  */
 public class MainClass
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
-        try
-        {
-            String switchString;
-            switchString = args[0];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String switchString;
 
-            if(args.length != 1)
+        while(true)
+        {
+
+            try
             {
-                System.out.println("Sorry, only one switch allowed");
-            }
-            else
-            {
-                switch(switchString)
+                switchString = args[0];
+
+                if(args.length != 1)
                 {
-                    case "hi":
-                        System.out.println("HELLO!");
-                        break;
-                    case "bye":
-                        System.out.println("BYEEEEEEEEEEEE!");
-
+                    System.out.println("Sorry, only one switch allowed");
                 }
+                else
+                {
+                    switch(switchString)
+                    {
+                        case "hi":
+                            System.out.println("HELLO!");
+                            break;
+                        case "bye":
+                            System.out.println("BYEEEEEEEEEEEE!");
+                            break;
+                        case "exit":
+                            System.out.println("CYAAAAAAAA");
+                            System.exit(0);
+                            break;
+                    }
+                }
+                args[0] = br.readLine();
+            }
+
+            catch(ArrayIndexOutOfBoundsException e)
+            {
+                System.out.println("The array is empty. Must enter at /n least one /n/t value.");
+                System.out.println(e.getMessage());
             }
         }
-        catch(NullPointerException e)
-        {
-            System.out.println("The array is empty. Must enter at /n least one /n/t value.");
-            System.out.println(e.getMessage());
-        }
-
     }
 }
 
